df305f5618a55a396781c05b202ad8c4bc682479
Riza Saputra <murdiyono@MacBook-Air-Riza.local>
2025-10-08 19:56:18 +0700

    test build

---
+++ vercel-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# 1. Pastikan Prisma Client digenerate (Membaca DATABASE_URL)
+npx prisma generate
+
+# 2. Jalankan build Next.js standar
+npm run build
